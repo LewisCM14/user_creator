1d530380c0960e6cbb7c1881c77370a917bc2af1
LewisCM14 <Lewiscmartin@live.co.uk>
2022-06-20 18:46:32 +0100

    handle addUser form validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -11,18 +11,49 @@ const AddUser = (props) => {
         Component specific CSS set via the AddUser.module.css file.
     */
 
-    // Used on the form element, Prevents the request being sent on form submission
+    // Initializes the enteredUsername state as a blank string
+    const [enteredUsername, setEnteredUsername] = useState('');
+
+    // Initializes the enteredAge state as a blank string
+    const [enteredAge, setEnteredAge] = useState('');
+
+    // Used on the form element, Prevents the request being sent on form submission.
     const addUserHandler = (event) => {
         event.preventDefault();
+        // If once either field is trimmed and the length is 0, function execution is broken with return.
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+        // If age value is less than 1 function execution is broken with return. 
+        // String to integer conversion forced with '+'
+        if (+enteredAge < 1) {
+            return;
+        }
+        // Logs the username and age before resetting the state to an empty string
+        console.log(enteredUsername, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     };
+
+    // Listens for the DOM onChange event for every key stoke on the 'username' input
+    // Calls the setEnteredUsername function, updating the enteredUsername state to the input value
+    // The target of the event is the input element and the value property of that input is the 'username'
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value)
+    };
+
+    // As above, for the age input.
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value)
+    }
 
     return (
         <Card className={styles.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor='username'>Username</label>
-                <input id='username' type='text'/>
+                <input id='username' type='text' value={enteredUsername} onChange={usernameChangeHandler}/>
                 <label htmlFor='age'>Age (Years)</label>
-                <input id='age' type='number'/>
+                <input id='age' type='number' value={enteredAge} onChange={ageChangeHandler}/>
                 <Button type='submit'>Add User</Button>
             </form>
         </Card>
